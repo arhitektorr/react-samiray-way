4bd7a199fc26ac3578e5172aa32481f5a5eb5750
KOSUMOV LOMALI <groznyprogrammer@gmail.com>
2026-02-24 15:45:52 +0300

    add env for api-keu

--- src/dal/api.ts
+++ src/dal/api.ts
@@ -14,9 +14,7 @@ export const getTrack = (trackId: string) => {
   const promise: Promise<GetTrackDetailsOutput> = fetch(
     `https://musicfun.it-incubator.app/api/1.0/playlists/tracks/${trackId}`,
     {
-      headers: {
-        // 'api-key': '5d63a8bd-090b-4072-88bf-f7b14b9121c2',
-      },
+      headers: prepareHeaders(),
     },
   ).then((res) => res.json());
 
@@ -40,14 +38,20 @@ export type TrackListItemOutput = {
 type GetTrackListOutput = {
   data: Array<TrackListItemOutput>;
 };
+const prepareHeaders = () => {
+  const apiKey = import.meta.env.VITE_API_KEY;
 
+  if (!apiKey) return undefined;
+
+  return {
+    'api-key': apiKey,
+  };
+};
 export const getTracks = () => {
   const promise: Promise<GetTrackListOutput> = fetch(
     'https://musicfun.it-incubator.app/api/1.0/playlists/tracks',
     {
-      headers: {
-        // 'api-key': '5d63a8bd-090b-4072-88bf-f7b14b9121c2',
-      },
+      headers: prepareHeaders(),
     },
   ).then((res) => res.json());
 
